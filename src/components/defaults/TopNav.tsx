e3c3c4d7f4fb879847bab984f2ec6e512c613832
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-08-02 18:30:28 +0100

    routes configured

---
+++ src/components/defaults/TopNav.tsx
@@ -0,0 +1,9 @@
+const TopNav = () => {
+  return (
+    <div>
+        <h2>Top Nav</h2>
+    </div>
+  )
+}
+
+export default TopNav
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import TopNav from "../../components/defaults/TopNav"
+
+const Home = () => {
+  return (
+    <div>
+        <TopNav />
+    </div>
+  )
+}
+
+export default Home
