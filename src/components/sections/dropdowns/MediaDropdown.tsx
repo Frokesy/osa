304112473cb10c28ed669ce540480371355e8fe8
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-08-11 07:29:07 +0100

    redesigned mediadropdown

--- src/components/sections/dropdowns/MediaDropdown.tsx
+++ src/components/sections/dropdowns/MediaDropdown.tsx
@@ -1,28 +1,45 @@
 import { motion } from "framer-motion";
 import { NavLink } from "react-router-dom";
+import { ServiceIcon } from "../../svgs/Icons";
 
 const MediaDropdown = () => {
-    const items = [
-        { id: 1, title: "News Highlight", link: "/media/highlights" },
-        { id: 2, title: "HR Nugget", link: "/media/nugget" },
-    ]
+  
+  const items = [
+    {
+      id: 1,
+      title: "News Highlight",
+      link: "/media/highlights",
+      desc: "The latest industry news, interviews, technologies, and resources.",
+      icon: <ServiceIcon size="48" color="#1F3887" />,
+    },
+    {
+      id: 2,
+      title: "HR Nugget",
+      link: "/media/nugget",
+      desc: "The latest industry news, interviews, technologies, and resources.",
+      icon: <ServiceIcon size="48" color="#1F3887" />,
+    },
+  ];
   return (
     <motion.div
       initial={{ opacity: 0, y: -10 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -10 }}
       transition={{ duration: 0.2 }}
-      className="lg:absolute bg-gray-800 text-[#fff] z-60 lg:rounded-xl lg:shadow-lg mt-2 flex flex-col p-2 space-y-2 lg:w-[20vw] w-[100%] lg:top-16"
+      className="lg:absolute bg-[#fff] w-[90vw] text-[#333] z-50 lg:rounded-xl left-0 lg:shadow-3xl lg:top-[17vh] lg:p-10 p-3"
     >
-      {items.map((item) => (
-        <NavLink
-          key={item.id}
-          to={item.link}
-          className="text-[15px] hover:bg-gray-600 hover:rounded-xl p-3 transition-all duration-300 ease-in-out lg:border-none border-b-2 border-[#404040]"
-        >
-          {item.title}
-        </NavLink>
-      ))}
+      <h2 className="text-[14px] text-[#1F3887] font-semibold mb-6">Media</h2>
+      <div className="gap-10 grid grid-cols-1 lg:grid-cols-3">
+        {items.map((item) => (
+          <NavLink key={item.id} to={item.link} className="flex space-x-3">
+            <div className="">{item.icon}</div>
+            <div className="">
+              <h2 className="">{item.title}</h2>
+              <p className="text-[#5D5E5F]">{item.desc}</p>
+            </div>
+          </NavLink>
+        ))}
+      </div>
     </motion.div>
   );
 };
