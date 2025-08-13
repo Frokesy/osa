4db827842001e03f48d0da68bb22ae29152e8aa3
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-08-13 05:50:02 +0100

    animated pms page

--- src/pages/services/pms/index.tsx
+++ src/pages/services/pms/index.tsx
@@ -1,4 +1,3 @@
-import { SettingsIcon } from "lucide-react";
 import TopNav from "../../../components/defaults/TopNav";
 import {
   PMSIcon,
@@ -83,7 +82,11 @@ const PerformanceManagementSystem = () => {
     <Container>
       <TopNav />
       <div className="w-[90vw] rounded-4xl lg:mt-14 mt-10 lg:mb-10 mx-auto">
-        <div className="flex items-center space-x-3">
+        <div
+          data-aos="fade-right"
+          data-aos-duration="1000"
+          className="flex items-center space-x-3"
+        >
           <div className="h-1 rounded-full w-8 bg-[#223D94]"></div>
           <p className="lg:text-[20px] uppercase font-semibold text-[#223D94]">
             Services
@@ -92,20 +95,36 @@ const PerformanceManagementSystem = () => {
 
         <div className="flex items-center justify-between lg:mt-0 ">
           <div className="w-[70%]">
-            <h2 className="lg:text-[48px] text-[18px] text-[#223D94] font-semibold uppercase">
+            <h2
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              className="lg:text-[48px] text-[18px] text-[#223D94] font-semibold uppercase"
+            >
               Recognition System
             </h2>
-            <p className="lg:text-[14px] text-[12px] pt-2 text-[#5D5E5F]">
+            <p
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              className="lg:text-[14px] text-[12px] pt-2 text-[#5D5E5F]"
+            >
               Creating structured programs to celebrate employee achievements
               and boost motivation.
             </p>
           </div>
-          <div className="w-[30%] lg:w-auto lg:scale-100 scale-50">
+          <div
+            data-aos="fade-left"
+            data-aos-duration="1000"
+            className="w-[30%] lg:w-auto lg:scale-100 scale-50"
+          >
             <PMSIcon />
           </div>
         </div>
       </div>
-      <div className="bg-[url('/assets/services-ten.png')] relative lg:h-[80vh] h-[50vh] bg-center bg-cover bg-no-repeat py-10 lg:px-10 flex items-center justify-end">
+      <div
+        data-aos="fade-up"
+        data-aos-duration="1000"
+        className="bg-[url('/assets/services-ten.png')] relative lg:h-[80vh] h-[50vh] bg-center bg-cover bg-no-repeat py-10 lg:px-10 flex items-center justify-end"
+      >
         <div className="space-y-4 bg-[#fff] lg:p-10 p-6 lg:relative absolute lg:bottom-0 mx-4 lg:mx-0 -bottom-[15vh] shadow-2xl rounded-lg lg:w-[50%]">
           <div className="flex items-center space-x-3">
             <ServiceIcon color="#223D94" size="40" />
@@ -115,7 +134,11 @@ const PerformanceManagementSystem = () => {
             </h2>
           </div>
 
-          <h2 className="pt-3 lg:text-[48px] text-[24px] font-semibold">
+          <h2
+            data-aos="fade-up"
+            data-aos-duration="2000"
+            className="pt-3 lg:text-[48px] text-[24px] font-semibold"
+          >
             "Tracking and improving employee performance to align with business
             goals."
           </h2>
@@ -123,11 +146,19 @@ const PerformanceManagementSystem = () => {
       </div>
 
       <div className="lg:mt-20 mt-[20vh] w-[90vw] mx-auto">
-        <SettingsIcon />
-        <h2 className="lg:text-[36px] text-[20px] font-semibold mt-10">
+        <PMSIcon size="60" />
+        <h2
+          data-aos="fade-down"
+          data-aos-duration="1000"
+          className="lg:text-[36px] text-[20px] font-semibold mt-10"
+        >
           Recognition System
         </h2>
-        <p className="lg:text-[20px] text-[14px] pt-6 text-[#1D2939]">
+        <p
+          data-aos="fade-down"
+          data-aos-duration="1000"
+          className="lg:text-[20px] text-[14px] pt-6 text-[#1D2939]"
+        >
           An adequate performance management system with defined competencies
           ensures an organization is able to manage the performance of its
           employees to achieve its strategic objective. Adequately defined KPIs
@@ -135,7 +166,11 @@ const PerformanceManagementSystem = () => {
           staff performance areas in line with the departments and the
           organization&apos;s set goals and objectives.
         </p>
-        <p className="lg:text-[20px] text-[14px] pt-6 text-[#1D2939]">
+        <p
+          data-aos="fade-down"
+          data-aos-duration="1000"
+          className="lg:text-[20px] text-[14px] pt-6 text-[#1D2939]"
+        >
           We defined a unique and bespoke methodology to evaluating performance
           within an organization. Our system ensures leadership can generate
           insights from analytics that seek to improve productivity and business
@@ -155,6 +190,8 @@ const PerformanceManagementSystem = () => {
             } flex justify-between items-center rounded-2xl lg:flex-row flex-col`}
           >
             <div
+              data-aos="fade-up"
+              data-aos-duration="1000"
               className={`${
                 item.id % 2 === 0 ? "px-3" : "px-6 lg:px-0"
               } lg:w-[70%] space-y-4 lg:pr-6 lg:pb-0 pb-10`}
